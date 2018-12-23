156cba9f3e869abc09e77265a3482fb164f8ef03
Kris_Chan <kris.dacpc@gmail.com>
2018-12-23 23:30:06 +0800

    1.调整架构;

---
+++ spider/run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: kris_peng
+# Created on 23/12/2018
+
+from config import MYSQL_DATABASE_URI
+from src import lagou
+
+if __name__ == '__main__':
+    lagou.main_task()
---
+++ spider/src/lagou.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: kris_peng
+# Created on 23/12/2018
+
+from 
+
+def main_task():
+    
